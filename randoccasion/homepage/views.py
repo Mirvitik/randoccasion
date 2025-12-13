fb724f5fff1452e76eba75f83075f0a23213cc74
Mirvitik <mirvitik@vk.com>
2025-12-13 19:39:16 +0300

    UPDATED: main page was changed for authorized and unauthorized users

--- randoccasion/homepage/views.py
+++ randoccasion/homepage/views.py
@@ -1,12 +1,39 @@
 __all__ = ()
 
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from events.models import Event
 
 
-class MainView(TemplateView):
-    template_name = "homepage/main.html"
+class MainView(ListView):
+    model = Event
+    context_object_name = "events"
+
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            return ["homepage/main.html"]
+
+        return ["homepage/guest_main.html"]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Event.objects.recommended_events_for_user(
+                self.request.user,
+            )[:6]
+
+        return None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = "Главная страница"
+
+        if self.request.user.is_authenticated:
+            context["title"] = "Главная страница"
+            context["user"] = self.request.user
+            context["friends_count"] = self.request.user.friends.count()
+        else:
+            context["title"] = "Добро пожаловать"
+            context["events_count"] = Event.objects.filter(
+                is_active=True,
+            ).count()
+
         return context
